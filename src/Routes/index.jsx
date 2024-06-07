6bb373dc9dd988113bea05e3688ef6fac161330e
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-06-07 16:46:02 -0400

    add the products router

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,13 +1,14 @@
 import { createBrowserRouter } from "react-router-dom"
 import Layout from "../layout"
 import App from "../App"
+import Products from "../Pages/Products"
 
  const router = createBrowserRouter([
     { path: '/',
         element: <Layout />,
         children : [
             { path: '/child', element: <App />, },
- 
+            { path: '/products', element: <Products />, },
         ]
     }
 ])
