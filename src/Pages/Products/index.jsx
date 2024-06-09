0faa4989efa90787cb73d8d04080734c58b6f736
LuisanaHenmary <luisanahenmaryp@gmail.com>
2024-06-09 18:02:52 -0400

    Added view product data

--- src/Pages/Products/index.jsx
+++ src/Pages/Products/index.jsx
@@ -1,15 +1,45 @@
 import {
     Box,
-    Typography
+    Typography,
+    TextareaAutosize,
+    styled
 } from "@mui/material"
 import { useEffect, useState } from "react"
 import ButtonAdd from "../../Components/ButtonAdd";
 import TableProducts from "../../Components/TableProducts";
 import RegisterProduct from "../../Components/RegisterProduct";
+import InfoCard from "../../layout/InfoCard"
+import Info from "../../Components/Info";
+
+const initialValues = {
+    name: '',
+    price: '',
+    acount: '',
+    description: '',
+};
+
+const Textarea = styled(TextareaAutosize)(() => `
+    width: 100%;
+  `,
+);
 
 const Products = () => {
     const [listProducts, setListProducts] = useState([]);
     const [add, setAdd] = useState(false);
+    const [view, setView] = useState(false);
+    const [selectInfo, setSelectInfo] = useState(initialValues)
+
+    const openView = (index) => {
+
+        const info = listProducts[index]
+        setSelectInfo(info)
+
+        setView(true);
+    }
+
+    const closeView = () => {
+        setView(false);
+    };
 
     const addProduct = (values) => {
 
@@ -40,9 +70,24 @@ const Products = () => {
                 Productos
             </Typography>
 
-            <ButtonAdd action={()=>setAdd(true)} >Agregar producto</ButtonAdd>
+            <ButtonAdd action={() => setAdd(true)} >Agregar producto</ButtonAdd>
             <RegisterProduct saveFunction={addProduct} isOpen={add} handleClose={() => setAdd(false)} />
-            <TableProducts products={listProducts} />
+            <TableProducts products={listProducts} openView={openView} />
+
+            <InfoCard name_section="Datos del producto" isOpen={view} handleClose={closeView}>
+                <Info name="Nombre del producto" value={selectInfo.name} />
+                <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <Info name="Precio" value={selectInfo.price} />
+                    <Info name="Cantidad" value={selectInfo.acount} />
+                </div>
+
+                <Info
+                    name="Descripción"
+                    value={<Textarea placeholder={selectInfo.description} disabled maxRows={4} />}
+                />
+
+
+            </InfoCard>
         </Box>
     )
 }
