819d78878d122992747286bec166c8fff3d3f930
Jesus Chacon <jesus.chacon@zalvadora.com>
2024-06-14 11:35:32 -0400

    creacion de rutas y vista de login y registro

---
+++ src/Pages/Login/index.jsx
@@ -0,0 +1,9 @@
+function Login() {
+  return (
+    <div>
+      <h1>Hello, World! Login</h1>
+    </div>
+  );
+}
+
+export default Login;
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,16 +1,30 @@
-import { createBrowserRouter } from "react-router-dom"
+import { createBrowserRouter, Navigate } from "react-router-dom"
 import Layout from "../layout"
 import App from "../App"
 import Products from "../Pages/Products"
 
- const router = createBrowserRouter([
-    { path: '/',
-        element: <Layout />,
-        children : [
-            { path: '/child', element: <App />, },
-            { path: '/products', element: <Products />, },
+import Login from '../Pages/Login'
+import Register from '../Pages/Register'
+import Dashboard from "../Pages/Dashboard"
+
+const router = createBrowserRouter([
+  { path: '/',
+    element: <Layout />,
+    children: [
+      { index: true, element: <Navigate to="login" replace />},
+      { path: 'login', element: <Login />, },
+      { path: 'register', element: <Register />, },
+      {
+        path: 'dashboard',
+        element: <Dashboard />,
+        children: [
+          { index: true, element: <Navigate to="child" replace /> },
+          { path: 'child', element: <App />, },
         ]
-    }
+      },
+      { path: '/products', element: <Products />, },
+    ]
+  }
 ])
 
 export default router
