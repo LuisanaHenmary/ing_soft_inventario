f916f09b711dfd445d83c92e375742c93595aa05
21harley <john.llanes.1996@gmail.com>
2024-06-18 00:24:23 -0400

    delete url

--- src/Components/ProtectedRoute /index.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-import { useAuthentication } from '../../store/useAuthentication'
-
-const ProtectedRoute = ({Dashboard}) => {
-  const { userID } = useAuthentication();
-  return userID ? <Dashboard><Outlet /></Dashboard> : <Navigate to="/login" />;
-};
-
-export default ProtectedRoute;
